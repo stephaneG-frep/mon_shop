447c3592d8e362239dbbc19a0ac9e7860e142d44
stephaneG-frep <119919153+stephaneG-frep@users.noreply.github.com>
2024-05-28 08:15:15 +0200

    user avec bddb et form

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +16,36 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('submit', SubmitType::class)
+            ->add('email', EmailType::class, [
+                'label' => "Votre Email",
+                'attr' => [
+                    'placeholder' => "Entrez votre Email exemple:dugenou@quiclac.com"
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => "Votre mot de passe",
+                'attr' => [
+                    'placeholder' => "Entrez votre passe"
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                 'label' => "Votre prénom",
+                 'attr' => [
+                    'placeholder' => "Entrez votre prénom"
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                 'label' => "votre nom",
+                 'attr' => [
+                    'placeholder' => "Entrez votre nom"
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Envoyer",
+                'attr' => [
+                   'style' => "background: #AFD198; color: #FBA834"
+                ]
+            ])
         ;
     }
 

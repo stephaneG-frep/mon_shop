60192d18e8f13e82087a92cc772c43ac18e3a137
stephaneG-frep <119919153+stephaneG-frep@users.noreply.github.com>
2024-06-09 22:57:21 +0200

    facture a imprimer pour le client et pour l'administrateur

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -12,12 +12,12 @@ class InvoiceController extends AbstractController
 {
 
     /*
-    *  IMPRESSION FACTURE PDF
+    *  IMPRESSION FACTURE PDF POUR LE CLIENT
     *  vérification de la commande 
     */
 
-    #[Route('/compte/facture/impression/{id_order}', name: 'app_invoice')]
-    public function index(OrderRepository $orderRepository, $id_order): Response
+    #[Route('/compte/facture/impression/{id_order}', name: 'app_invoice_customer')]
+    public function printForCustomer(OrderRepository $orderRepository, $id_order): Response
     {
 
         // 1. Vérification de l'objet commande  ---Existe?
@@ -46,6 +46,40 @@ class InvoiceController extends AbstractController
        ]);
 
            exit();
+    }
 
+
+    
+    /*
+    *  IMPRESSION FACTURE PDF POUR L'ADMINISTRATEUR
+    *  vérification de la commande 
+    */
+
+    #[Route('/admin/facture/impression/{id_order}', name: 'app_invoice_admin')]
+    public function printForAdmin(OrderRepository $orderRepository, $id_order): Response
+    {
+
+        // 1. Vérification de l'objet commande  ---Existe?
+        $order = $orderRepository->findOneById($id_order);
+        if (!$order) {
+            return $this->redirectToRoute('admin');
+        }
+
+         // générer des facture au format PDF avec le bundle DomPdf
+        $dompdf = new Dompdf();
+      // pour revoyer la view (vue)
+        $html = $this->renderView('invoice/index.html.twig', [
+            'order' => $order,
+        ]);
+        $dompdf->loadHtml($html);
+        // Format et orientation
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        // pour ouvrir le fichier dans le navigateur
+        $dompdf->stream('facture.pdf', [
+             'Attachment' => false,
+       ]);
+
+           exit();
     }
 }
